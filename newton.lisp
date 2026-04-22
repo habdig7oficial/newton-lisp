7ffb850ad0992707f6d698cbd54290ad118f8c71
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-04-22 15:05:47 -0300

    save this

--- newton.lisp
+++ newton.lisp
@@ -11,15 +11,42 @@
 
 
 (defun derivate-all (fx)
-  (print fx)
+  ;(print fx)
   (cond
     ((null fx) nil)
     ((atom fx) fx)
     (t (cons (derivate (derivate-all (car fx))) (derivate-all (cdr fx))))))
   
+(defun rel-err (value aprox)
+  (/ (abs (- value aprox)) value))
+
+(defun newton (fx p epsilon &optional (var 'x))
+  (let* (
+	 (fdx (derivate-all fx))
+	 (a   (subst p var fx))
+	 (b   (subst p var fdx))
+	 )
+    (format t "f(x)  = ~a~%f'(x) = ~a ~%" fx fdx)
+    a))
 
 
-(defun newton (fx p epsilon))
+(defparameter *episilon* 0.00001)
+
+(let ((b '(+ (log (- x 1)) (cos (- x 1)))))
+  (print (newton b 1.3 *episilon*))
+  (print (rel-err 0.1 0.15)))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ;(print (eval (subst 3 'x (derivate '(expt 'x 3)))))
@@ -28,13 +55,4 @@
 ;(print (derivate 2))
 
 ;(let ((a '(- (+ (exp x) (expt 2 (- x)) (* 2 (cos x))) 6))))
-
-(let ((b '(+ (log (- x 1)) (cos (- (log (expt x 2)) 1)))))
-  (print (derivate-all b))
-  (print (derivate '(expt x 3))))
-
-
-
-
-
 	  
