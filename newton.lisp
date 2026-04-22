00de8f56a32542b0f4e5ec575fdc3286ab87de4e
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-04-22 15:34:37 -0300

    err

--- newton.lisp
+++ newton.lisp
@@ -20,21 +20,35 @@
 (defun rel-err (value aprox)
   (/ (abs (- value aprox)) value))
 
-(defun newton (fx p epsilon &optional (var 'x))
+(defun newton* (fx fdx p epsilon i var)
   (let* (
-	 (fdx (derivate-all fx))
-	 (a   (subst p var fx))
-	 (b   (subst p var fdx))
-	 )
-    (format t "f(x)  = ~a~%f'(x) = ~a ~%" fx fdx)
-    a))
+	 (fa   (subst p var fx))
+	 (a (eval fa))
+	 (fb   (subst p var fdx))
+	 (b (eval fb))
+	 (ab (/ a b))
+	 (pnext (- p ab))
+	 (err (eval (subst pnext var fx))))
+    (format t "~%====================~%~%")
+    (format t "f(~a)  = ~a = ~a~%f'(~a) = ~a = ~a~%" p fa a p fb b)
+    (format t "f(~a) / f'(~a) = ~a~%" p p ab)
+    (format t "~%p~a = p~a - f(~a) / f'(~a)~%" (+ i 1) i p p)
+    (format t "~%~a = ~a - ~a~%" pnext p ab)
+    (format t "Iteration Err: ~a~%" err)
+    (format t "~%====================~%")
+  ))
+
+(defun newton (fx p epsilon &optional (var 'x))
+  (let* ((fdx (derivate-all fx)))
+    (format t "f(x)  = ~a~%f'(x) = ~a ~%~%~%" fx fdx)
+    (newton* fx fdx p epsilon 0 var)))
 
 
 (defparameter *episilon* 0.00001)
 
 (let ((b '(+ (log (- x 1)) (cos (- x 1)))))
   (print (newton b 1.3 *episilon*))
-  (print (rel-err 0.1 0.15)))
+  )
 
 
 
